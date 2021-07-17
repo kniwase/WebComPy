2a1f319cf0c55d4c68c2179339c3c3187e805da1
Kento Niwase <kento.niwase@outlook.com>
2021-07-17 19:15:14 +0900

    add typings for dev

---
+++ typings/browser/__init__.pyi
@@ -0,0 +1,12 @@
+from typing import Any
+
+document: Any
+aio: Any
+ajax: Any
+html: Any
+webcomponent: Any
+window: Any
+javascript: Any
+template: Any
+markdown: Any
+load: Any
---
+++ typings/javascript/__init__.pyi
@@ -0,0 +1,4 @@
+from typing import Any
+
+RegExp: Any
+String: Any
