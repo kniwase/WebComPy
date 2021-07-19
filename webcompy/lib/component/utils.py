958186cce7b9a73dd3d0de64f7f8ac0d2ef468da
Kento Niwase <kento.niwase@outlook.com>
2021-07-20 00:46:59 +0900

    set type

--- webcompy/lib/component/utils.py
+++ webcompy/lib/component/utils.py
@@ -1,5 +1,6 @@
-from typing import Any, Type
+from typing import Type
 from javascript import RegExp, String
+from .base import WebcompyComponent
 
 
 pattern = RegExp.new(r'[A-Z]+', 'g')
@@ -13,11 +14,11 @@ def convert_camel_to_kebab(text: str):
     return String.new(text).replace(pattern, replacer).strip('-')
 
 
-def get_component_class_name(component: Type[Any]) -> str:
+def get_component_class_name(component: Type[WebcompyComponent]) -> str:
     return component.__bases__[-1].__name__
 
 
-def get_component_tag_name(component: Type[Any]) -> str:
+def get_component_tag_name(component: Type[WebcompyComponent]) -> str:
     class_name = get_component_class_name(component)
     tag_name = convert_camel_to_kebab(class_name)
     return tag_name
