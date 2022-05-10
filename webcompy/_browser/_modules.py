bfd6b3243d9f6bade3bb6d4bed36c0414c310113
Kento Niwase <kento.niwase@outlook.com>
2022-05-10 12:11:00 +0900

    fix var name

--- webcompy/_browser/_modules.py
+++ webcompy/_browser/_modules.py
@@ -5,12 +5,12 @@ if _ENVIRONMENT == "pyscript":
     from webcompy._browser._pyscript import _module as browser_pyscript
 
     browser = browser_pyscript
-    brython = None
+    browser_brython = None
 elif _ENVIRONMENT == "brython":
     from webcompy._browser._brython import _module as browser_brython
 
     browser = browser_brython
-    pyscript = None
+    browser_pyscript = None
 else:
     browser_pyscript = None
     browser_brython = None
