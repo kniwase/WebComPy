5ad23a8c2311fb06758e054e548b5fab4b191cff
Kento Niwase <kento.niwase@outlook.com>
2022-06-23 11:29:03 +0900

    change sample code displaying method

--- docs_src/pages/demo/helloworld_classstyle.py
+++ docs_src/pages/demo/helloworld_classstyle.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from webcompy.elements import html
 from webcompy.components import define_component, ComponentContext
 from webcompy.router import RouterContext
@@ -9,32 +10,14 @@ from ...templates.demo.helloworld_classstyle import HelloWorldClassstyle
 def HelloWorldClassstylePage(context: ComponentContext[RouterContext]):
     title = "HelloWorld (ClassStyle)"
     context.set_title(f"{title} - WebCompy Demo")
+    code = Path(__file__).parents[2] / "templates/demo/helloworld_classstyle.py"
 
     return html.DIV(
         {},
         DemoDisplay(
             {
                 "title": title,
-                "code": """
-                    from webcompy.elements import html
-                    from webcompy.components import (
-                        TypedComponentBase,
-                        component_class,
-                        component_template,
-                    )
-
-
-                    @component_class
-                    class HelloWorldClassstyle(TypedComponentBase(props_type=None)):
-                        @component_template
-                        def template(self):
-                            return html.DIV(
-                                {},
-                                html.H1(
-                                    {},
-                                    "Hello WebComPy!",
-                                ),
-                            )""",
+                "code": code.open().read().strip(),
             },
             slots={"component": lambda: HelloWorldClassstyle(None)},
         ),
